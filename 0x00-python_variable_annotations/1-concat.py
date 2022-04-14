4fef436f6fd27255042042287c594aa76a0d23a3
Yohannes Terfa <lightshadow011@gmail.com>
2022-04-14 22:01:00 +0300

    fn type annotated concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+Basic annotations - concat
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    a function that just concatenates
+    but is type annotated
+    '''
+    return str1 + str2
