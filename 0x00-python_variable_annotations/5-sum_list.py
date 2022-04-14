1e7f81cd7c83477874e00d5f50a2ba6913d87cfd
Yohannes Terfa <lightshadow011@gmail.com>
2022-04-14 22:26:52 +0300

    type annotated complex

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+Annotation complex [list]
+'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    '''
+    sum of list values annotated
+    '''
+    return sum(input_list)
