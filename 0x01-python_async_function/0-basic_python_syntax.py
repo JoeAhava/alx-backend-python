1f1be0a8b423bea67bf104369e384af648b8f560
Yohannes Terfa <lightshadow011@gmail.com>
2022-04-20 22:14:58 +0300

    first tas basic syntax asycn await

---
+++ 0x01-python_async_function/0-basic_python_syntax.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+'''
+Async Python Basic syntax
+'''
+
+
+import random
+import time
+import asyncio
+
+
+async def wait_random(max_delay=10):
+    '''
+    wait randomly between 0 and max_delay
+    '''
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
+
+if __name__ == '__main__':
+    print(asyncio.run(wait_random()))
+    print(asyncio.run(wait_random(5)))
+    print(asyncio.run(wait_random(15)))
